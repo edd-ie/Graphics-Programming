77fb327771feab6d18fb166cafa6519ae1e32856
edd-ie <ogendoeddie@gmail.com>
2025-01-23 22:51:02 -0400

    Creating an OpenGL context

--- Display/Window.cpp
+++ Display/Window.cpp
@@ -15,19 +15,40 @@ bool Window::init(const unsigned int width, const unsigned int height, const std
     mainWindow = glfwCreateWindow(static_cast<int>(width), static_cast<int>(height),
     title.c_str(), nullptr, nullptr);
 
+
     if (!mainWindow) {
         Logger::log(1, "%s: Could not create window\n", __FUNCTION__);
         glfwTerminate();
         return false;
     }
+
+    // Adding OpenGL context to the window
+    glfwMakeContextCurrent(mainWindow);
+
     Logger::log(1, "%s: Window successfully initialized\n", __FUNCTION__);
     return true;
 }
 
 void Window::mainLoop() {
+    // Enable vertical sync to prevent tearing & flickering
+    glfwSwapInterval(1);
+
+    // Background colour
+    float colour = 0.0f;
+
     while (!glfwWindowShouldClose(mainWindow)) {
+
+        colour >= 1.0f ? colour = 0.0f : colour += 0.01f;
+        glClearColor(colour, colour, colour, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Swap buffers
+        glfwSwapBuffers(mainWindow);
+
         /* poll events in a loop */
         glfwPollEvents();
+
+
     }
 }
 
---
+++ cmake-build-debug/CMakeFiles/fc-tmp/glfw/update.cmake
@@ -0,0 +1,9 @@
+cmake_minimum_required(VERSION 3.29)
+
+message(VERBOSE "Executing update step for glfw")
+
+block(SCOPE_FOR VARIABLES)
+
+include("X:/Code/C++/OpenGL/cmake-build-debug/CMakeFiles/fc-tmp/glfw/glfw-gitupdate.cmake")
+
+endblock()
