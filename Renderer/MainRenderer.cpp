afb1751288394aeffb9c94952c53d4e29d630e10
edd-ie <ogendoeddie@gmail.com>
2025-02-07 23:32:01 -0400

    OpenGL render pipeline

---
+++ Renderer/MainRenderer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by _edd.ie_ on 07/02/2025.
+//
+
+#include "MainRenderer.h"
---
+++ Renderer/MainRenderer.h
@@ -0,0 +1,35 @@
+//
+// Created by _edd.ie_ on 07/02/2025.
+//
+
+#ifndef MainRENDERER_H
+#define MainRENDERER_H
+
+#pragma once
+#include <vector>
+#include <string>
+#include <glm/glm.hpp>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include "Framebuffer.h"
+#include "VertexBuffer.h"
+#include "Texture.h"
+#include "Shader.h"
+#include "MainRenderData.h"
+
+class MainRenderer {
+    Shader basicShader{};
+    Framebuffer frameBuffer{};
+    VertexBuffer vertexBuffer{};
+    Texture texture{};
+    int triangleCount = 0;
+public:
+    bool init(unsigned int width, unsigned int height);
+    void setSize(unsigned int width, unsigned int height);
+    void cleanup();
+    void uploadData(OGLMesh vertexData);
+    void draw();
+};
+
+#endif //MainRENDERER_H
